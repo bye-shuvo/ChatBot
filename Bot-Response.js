957b22113301dd83249e7d424d65aa7ff16fe72f
bye-shuvo <abuhasnatshuvo605@gmail.com>
2024-09-15 18:48:53 +0600

    Added necessary files of the chatBot

---
+++ Bot-Response.js
@@ -0,0 +1,45 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const sendBtn = document.getElementById("send");
+const userInput = document.getElementById("input");
+const massageBox = document.getElementById("massageBox");
+
+let api_key;
+
+fetch("secure.json").then(response => response.json()).then(data => {
+    api_key = data.API_KEY;
+})
+
+function addBotMassage(botResponse) {
+    const ThinkingTextElement = document.createElement("div");
+    ThinkingTextElement.classList.add("bot");
+    ThinkingTextElement.innerHTML = `<img src="robot-solid.svg" alt="bot"><p class="botText">Thinking...</p>`;
+    massageBox.appendChild(ThinkingTextElement);
+    setTimeout(() => {
+        ThinkingTextElement.remove();
+        let messageElement = `<div class="bot"><img src="robot-solid.svg" alt="bot"><p class="botText">${botResponse}</p></div>`;
+        massageBox.insertAdjacentHTML("beforeend", messageElement);
+    }, 600);
+}
+
+async function botResponse() {
+    const genAI = new GoogleGenerativeAI(api_key);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    let userMessage = userInput.value.trim();  
+    const result = await model.generateContent(userMessage);
+    let botResponse = await result.response.text();
+    addBotMassage(botResponse);
+}
+
+function sendMassege() {
+    sendBtn.addEventListener("click", () => {
+        if (userInput.value != "") {
+            botResponse();
+            userInput.value = "";
+        }
+        else {
+            alert("Enter a prompt.");
+        }
+    })
+}
+sendMassege();
---
+++ script.js
@@ -0,0 +1,51 @@
+const openButton = document.getElementById("chatBoxOpen");
+const closeButton = document.getElementById("chatBoxClose");
+const chatBot = document.getElementById("chatBox");
+const massageBox = document.getElementById("massageBox");
+const sendBox = document.getElementById("sendBox");
+const botMessege = document.getElementsByClassName("bot");
+const userMessege = document.getElementsByClassName("user");
+const sendBtn = document.getElementById("send");
+const userInput = document.getElementById("input");
+
+function openBot() {
+    openButton.addEventListener("click", () => {
+        openButton.classList.remove("openChatBot");
+        openButton.classList.add("closeChatBot");
+        closeButton.classList.add("openChatBot");
+        closeButton.classList.remove("closeChatBot");
+        chatBot.classList.remove("closeChatBot");
+        chatBot.classList.add("openChatBot");
+    })
+}
+openBot();
+function closeBot() {
+    closeButton.addEventListener("click", () => {
+        closeButton.classList.remove("openChatBot");
+        closeButton.classList.add("closeChatBot");
+        openButton.classList.remove("closeChatBot");
+        openButton.classList.add("openChatBot");
+        chatBot.classList.remove("openChatBot");
+        chatBot.classList.add("closeChatBot");
+    })
+}
+closeBot();
+
+function sendMassege() {
+    sendBtn.addEventListener("click", () => {
+        if (userInput.value != "") {
+            addMassege();
+        }
+        else {
+            alert("Enter a prompt.");
+        }
+    })
+}
+sendMassege();
+
+function addMassege() {
+    let massage = userInput.value.trim();
+    let messegeElement = `<div class="user"><p class="userText">${massage}</p><img src="user-solid.svg" alt="user"></div>`;
+    massageBox.insertAdjacentHTML("beforeend", messegeElement);
+}
+
